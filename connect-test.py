c4dd451de36dbd3486779aea6c72c45f4d690863
R. Kyle Bocinsky <bocinsky@gmail.com>
2018-12-11 16:13:41 -0700

    trying to set up circleci

--- connect-test.py
+++ connect-test.py
@@ -1,5 +1,9 @@
 from os import getenv
 from zentra.api import *
 
-print(Session().send(ZentraToken().build(username=getenv("zentra_un"),
-                                         password=getenv("zentra_pw")).request))
+request = ZentraToken().build(username=getenv("zentra_un"),
+                              password=getenv("zentra_pw"))
+
+print(request.request)
+
+print(Session().send(request.request))
